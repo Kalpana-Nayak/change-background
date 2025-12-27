7ff1ac89a4aad06fa941ea402ef9e56d2528e1de
Kalpana Nayak <156688653+Kalpana-Nayak@users.noreply.github.com>
2025-12-27 09:37:18 +0000

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # change-background
 Choose color and change the background of your own choice
+A web page showcasing the use of async function such as setInterval
---
+++ script.js
@@ -0,0 +1,27 @@
+// generate random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = "#"
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+let intervalId = null
+
+const startChangingColor = function(){
+    if(intervalId === null){
+    intervalId = setInterval(() => {
+            document.body.style.backgroundColor = randomColor()
+        }, 1000)
+    }
+}
+
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+document.querySelector("#start").addEventListener("click", startChangingColor)
+document.querySelector("#stop").addEventListener("click", stopChangingColor)
